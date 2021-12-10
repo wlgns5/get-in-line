5c6de2e2dc722b040cc69b05e9c22d857587e594
Lavine <zihun35@gmail.com>
2021-12-10 13:15:36 +0900

    Spring boot setup

---
+++ src/main/java/com/getInLine/lavine/LavineApplication.java
@@ -0,0 +1,13 @@
+package com.getInLine.lavine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getInLine/lavine/LavineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getInLine.lavine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

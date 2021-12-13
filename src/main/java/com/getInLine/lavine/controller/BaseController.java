ccd12904d91a7151c317ff115d0a68dc45ab36b3
Lavine <zihun35@gmail.com>
2021-12-13 14:22:17 +0900

    BaseController 수정 (에러 페이지 제거)

--- src/main/java/com/getInLine/lavine/controller/BaseController.java
+++ src/main/java/com/getInLine/lavine/controller/BaseController.java
@@ -4,14 +4,9 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class BaseController implements ErrorController {
+public class BaseController {
     @GetMapping("/")
     public String root() {
         return "index";
-    }
-
-    @RequestMapping("/error")
-    public String error() {
-        return "error";
     }
 }
